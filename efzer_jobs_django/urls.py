0afe35cc4cb8f40145f81f9e9897be8df2832192
Kyle Yan <kaicheng.yan.1997@gmail.com>
2017-09-04 09:17:18 +0800

    map root to jobs

--- efzer_jobs_django/urls.py
+++ efzer_jobs_django/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.http import HttpResponseRedirect
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^jobs/', include('jobs.urls'))
+    url(r'^jobs/', include('jobs.urls')),
+    url(r'^$', lambda r: HttpResponseRedirect('jobs/')),
 ]
